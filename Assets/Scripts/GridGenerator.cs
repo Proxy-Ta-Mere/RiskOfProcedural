c713725f4c632bb4501e6a6f7533445351f03108
Loïc Pinsard <muream@gmail.com>
2020-06-08 22:55:32 +0100

    WIP: generate triangles for the grid

--- Assets/Scripts/GridGenerator.cs
+++ Assets/Scripts/GridGenerator.cs
@@ -16,38 +16,39 @@ public class GridGenerator
     Vector3[] vertices;
     int[] triangles;
 
-    private int circleCount;
 
     public GridGenerator(Mesh mesh, int circleResolution, int gridResolution)
     {
         this.mesh = mesh;
         this.circleResolution = circleResolution;
         this.gridResolution = gridResolution;
-        circleCount = gridResolution + 1;
 
         vertices = new Vector3[circleResolution + 1 + (gridResolution * circleResolution)];
-        //triangles = new int[circleResolution * stepCircleIndex * 3];
+
+        triangles = new int[(gridResolution + 1) * (gridResolution + 1) * circleResolution * 3];
     }
 
     public void ConstructMesh()
     {
-        Vector3[] temp_vertices = new Vector3[0];
+        Vector3[] tempVertices = new Vector3[0];
+        float radiusStep = 1f / (gridResolution + 1);
         for (int i = 0; i <= gridResolution; i++)
         {
-            float radius = 1f / (i + 1f);
-            Debug.Log(radius);
-            Vector3[] current_vertices = GeneratePointsOnPolygon(radius, gridResolution - i);
-            temp_vertices = temp_vertices.Concat(current_vertices).ToArray();
+            float radius = 1 - radiusStep * (i);
+            Vector3[] currentVertices = GeneratePointsOnPolygon(radius, gridResolution - i);
+            tempVertices = tempVertices.Concat(currentVertices).ToArray();
         }
 
+        // Array of one point containing the center vertex
         Vector3[] centerGrid = new Vector3[1] { Vector3.zero };
-        vertices = temp_vertices.Concat(centerGrid).ToArray();
 
-        //GenerateTriangles();
-        //mesh.Clear();
-        //mesh.vertices = vertices;
-        //mesh.triangles = triangles;
-        //mesh.RecalculateNormals();
+        vertices = tempVertices.Concat(centerGrid).ToArray();
+
+        GenerateTriangles();
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
     }
 
     private Vector3[] GeneratePointsOnPolygon(float radius, int nbInterpolatedPoints)
@@ -109,33 +110,63 @@ public class GridGenerator
     private void GenerateTriangles()
     {
         int triIndex = 0;
-        int inbetweenCount = gridResolution;
-        int vertexOffset = 0;
-        for (int i = 0; i < circleCount; i++)
+        int firstVertex = 0;
+        int currentGridResolution = gridResolution;
+        int centerVertexIndex = vertices.Length - 1;
+        for (int i = 0; i <= gridResolution; i++) // for each circle
         {
-            inbetweenCount -= i;
-            int vertexCount = circleResolution + circleResolution * inbetweenCount;
-
-            for (int j = 0; j < vertexCount; j++)
+            int vertexCount = circleResolution + circleResolution * currentGridResolution;
+            int nextCircleVertex = vertexCount;
+            for (int j = 0; j < vertexCount; j++) // for each vertex of circle
             {
-                int vertexIndex = j + vertexOffset;
+                int vertexIndex = firstVertex + j;
 
-                // create every triangle that have 2
-                int vertex1 = vertexIndex;
-                int vertex2 = vertexIndex + vertexCount;
-                int vertex3 = vertexIndex + 1;
-                if (j == vertexCount - 1)
-                    vertex3 = vertexOffset;
+                if (i == gridResolution)
+                {
+                    if (vertexIndex == vertexCount - 1) // if last point, last triangle point is the first vertex of the circle
+                    {
+                        triangles[triIndex] = vertexIndex;
+                        triangles[triIndex + 1] = centerVertexIndex;
+                        triangles[triIndex + 2] = firstVertex;
+                        triIndex += 3;
+                    }
+                    else // every circle vertex except last one
+                    {
+                        triangles[triIndex] = vertexIndex;
+                        triangles[triIndex + 1] = centerVertexIndex;
+                        triangles[triIndex + 2] = vertexIndex + 1;
+                        triIndex += 3;
+                    }
+                }
+                else
+                {
+                    if (vertexIndex == vertexCount - 1) // if last point, last triangle point is the first vertex of the circle
+                    {
+                        triangles[triIndex] = vertexIndex;
+                        triangles[triIndex + 1] = vertexIndex + nextCircleVertex;
+                        triangles[triIndex + 2] = firstVertex;
+                        triIndex += 3;
+                    }
+                    else // every circle vertex except last one
+                    {
+                        triangles[triIndex] = vertexIndex;
+                        triangles[triIndex + 1] = vertexIndex + nextCircleVertex;
+                        triangles[triIndex + 2] = vertexIndex + 1;
+                        triIndex += 3;
+                    }
 
-                triangles[triIndex] = vertex1;
-                triangles[triIndex + 1] = vertex2;
-                triangles[triIndex + 2] = vertex3;
-                triIndex += 3;
-
-                
+                    if (vertexIndex % (currentGridResolution + 1) != 0) // not a corner vertex
+                    {
+                        triangles[triIndex] = vertexIndex;
+                        triangles[triIndex + 1] = vertexIndex + nextCircleVertex - 1;
+                        triangles[triIndex + 2] = vertexIndex + nextCircleVertex;
+                        triIndex += 3;
+                        nextCircleVertex++;
+                    }
+                }
             }
-            vertexOffset += vertexCount;
-            
+            firstVertex += vertexCount;
+            currentGridResolution--;
         }
     }
 }
